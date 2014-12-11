85a30460fbaa4bc9f87f5a52e0364e879ed8eb48
figurebelow <ruben@figurebelow.com>
2014-12-11 01:56:29 +0000

    Added SVG circle ement

---
+++ svg/03-circle.js
@@ -0,0 +1,10 @@
+module.exports = function(RED) {
+    function circleNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        this.on('input', function(msg) {
+        msg.payload = circle = \"<circle cx="50\" cy=\"50\" r=\"40\" stroke=\"black\" stroke-width=\"3\" fill=\"red\" />";
+         node.send(msg);
+    };
+    RED.nodes.registerType("circle", circleNode);
+}
