dee5c991c188083904c1558c2dff5e87dd89efc0
figurebelow <ruben@figurebelow.com>
2014-12-12 00:11:47 +0000

    Fixed proof of concept

--- svg/03-circle.js
+++ svg/03-circle.js
@@ -3,8 +3,9 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         this.on('input', function(msg) {
-        msg.payload = circle = \"<circle cx="50\" cy=\"50\" r=\"40\" stroke=\"black\" stroke-width=\"3\" fill=\"red\" />";
-         node.send(msg);
+        msg.payload = "<circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"black\" stroke-width=\"3\" fill=\"red\" />";
+        node.send(msg);
+        });
     };
     RED.nodes.registerType("circle", circleNode);
 }
