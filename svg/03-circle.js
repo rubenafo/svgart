8f63f381d84379f4cb131101045a95038b893909
figurebelow <ruben@figurebelow.com>
2014-12-23 00:54:14 +0000

    Circles now understand nrSvg input field

--- svg/03-circle.js
+++ svg/03-circle.js
@@ -3,8 +3,15 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         this.on('input', function(msg) {
-        msg.payload = "<circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"black\" stroke-width=\"3\" fill=\"red\" />";
-        node.send(msg);
+          if (msg.nrSvg && msg.nrSvg.coords) {
+            var elems = new Array();
+            msg.nrSvg.coords.forEach (function (elem) {
+              var circle = "<circle cx=\"" + elem.x + "\" cy=\"" + elem.y + "\" r=\"40\" stroke=\"black\" stroke-width=\"3\" fill=\"red\" />";
+              elems[elems.length] = circle;
+            });
+            msg.payload = elems;
+            node.send (msg);
+          }
         });
     };
     RED.nodes.registerType("circle", circleNode);
