dbd4e0720b534c4d6664fe1b7ecda193a9a43828
figurebelow <ruben@figurebelow.com>
2014-12-24 02:56:04 +0100

    Group added

---
+++ svg/06-group.js
@@ -0,0 +1,34 @@
+module.exports = function(RED) {
+    var vm = require ("vm");
+    function GroupFunction (config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        var context = vm.createContext ();
+        context.count = context.count || 0;
+        context.data = context.data || [];
+        var elemsToGroup = config.elems;
+        this.on('input', function(msg) {
+          if (msg.nrSvg) {
+            var svgHeaderBegin = "<g>";
+            var svgHeaderEnd = "</g>";
+            if (context.count < elemsToGroup) {
+              node.send (null);
+	      console.log ("waiting for " + elemsToGroup);
+              context.data.push (msg.nrSvg);
+              context.count = context.count + 1;
+            }
+            if (context.count == elemsToGroup) {
+              msg.payload = svgHeaderBegin;
+              context.data.forEach (function (elem) {
+                msg.payload += elem;
+              });
+              msg.payload += svgHeaderEnd;
+              node.send(msg);
+              context.count = 0;
+              context.data = [];
+            }
+          }
+       });
+    };
+    RED.nodes.registerType("group", GroupFunction);
+}
