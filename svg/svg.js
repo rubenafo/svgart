8cf8a5e690911344ca529f357e03ba89cb40ba65
figurebelow <ruben@figurebelow.com>
2015-01-03 01:53:42 +0000

    Added text base implementation

--- svg/svg.js
+++ svg/svg.js
@@ -262,6 +262,37 @@ Line.adapt = function (elem) {
   return SVGBase.prototype.adapt.call (elem, Line.prototype);
 }
 
+// Line element /////////////////////
+
+Text.type = "text";
+Text.prototype = new SVGBase ();
+Text.prototype.parent = SVGBase.prototype;
+
+Text.prototype.constructor = Text;
+function Text (x, y, text, style, zindex) {
+  SVGBase.call (this, Text.type, zindex);
+  this.setPos (x, y);
+  this.setString (text);
+  this.parent.setAttribute.call (this, "style", style);
+};
+
+Text.prototype.setString = function (text) {
+  this.content.string = text;
+};
+
+Text.prototype.setPos = function (x,y) {
+  this.parent.setAttribute.call (this, "x", x); 
+  this.parent.setAttribute.call (this, "y", y);
+};
+
+Text.prototype.toSVG = function () {
+  return this.parent.toSVG.call (this, this.content.string);
+};
+
+Text.adapt = function (elem) {
+  return SVGBase.prototype.adapt.call (elem, Ellipse.prototype);
+}
+
 // SVG Adapter /////////////////////////
 
 SVGadapter = function (elem) {
