eff3cea1d9e957fc84b1ff5f9f387defc637be82
figurebelow <ruben@figurebelow.com>
2015-11-06 06:40:37 +0000

    Added Ellipse docs

--- nodes/svg/Ellipse.js
+++ nodes/svg/Ellipse.js
@@ -20,6 +20,16 @@ Ellipse.type = "ellipse";
 Ellipse.prototype = new SVGBase ();
 Ellipse.prototype.parent = SVGBase.prototype;
 
+/**
+* @class The SVG Ellipse element
+* @constructor Ellipse
+* @param {number} cx - x coord
+* @param {number} cy - y coord
+* @param {number} rx - x-radio value
+* @param {number} ry - y-radio value
+* @param {string} style - CSS style string
+* @param {number} zindex - zindex value
+*/
 Ellipse.prototype.constructor = Ellipse;
 function Ellipse (cx, cy, rx, ry, style, zindex) {
   SVGBase.call (this, Ellipse.type, zindex);
@@ -29,17 +39,29 @@ function Ellipse (cx, cy, rx, ry, style, zindex) {
   this.parent.setAttribute.call (this, "style", style);
 };
 
+/**
+* Sets the position of the ellipse
+* @param {number} x - x coord
+* @param {number} y - y coord
+*/
 Ellipse.prototype.setPos = function (x,y) {
-  this.parent.setAttribute.call (this, "cx", x); 
+  this.parent.setAttribute.call (this, "cx", x);
   this.parent.setAttribute.call (this, "cy", y);
 };
 
+/**
+* Returns the coords of the center of the ellipse
+* @returns - {x:value,y:value} containing the coords
+*/
 Ellipse.prototype.getCenter = function () {
 	var x = this.parent.getAttribute ("cx");
 	var y = this.parent.getAttribute ("cy");
 	return ({x:x/2,y:y/2});
 }
 
+/**
+* Adapts an object to the Ellipse class
+*/
 Ellipse.adapt = function (elem) {
   return SVGBase.prototype.adapt.call (elem, Ellipse.prototype);
 }
