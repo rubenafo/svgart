547b5be3da4ec0561c3f0415a76e84a75b6ae770
figurebelow <ruben@figurebelow.com>
2016-01-21 09:02:17 +0000

    Added missing test files

---
+++ nodes/svg/tests/TextTest.js
@@ -0,0 +1,7 @@
+/**
+* @license
+* Copyright 2016 Ruben Afonso, ruben@figurebelow.com
+*
+* This source code is licensed under the Apache license found in the
+* LICENSE file in the root directory of this source tree.
+**/
